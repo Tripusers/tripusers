e3b15fb91eb191ce325c6e1d136514b680e71135
Your Name <you@example.com>
2024-05-02 18:27:39 +0530

    update size

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,7 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/favicon.ico",
         type: "image/x-icon",
         purpose: "badge",
-        sizes: "16x16",
+        sizes: "48x48",
       },
     ],
   };
